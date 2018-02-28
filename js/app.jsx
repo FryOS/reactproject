bcc513a2aeb2082c2cce715ddd13f094d7ca3b59
FryOS <alexeyosp@gmail.com>
2018-02-28 16:49:27 +0300

    Создал новый компонент с изменением состояния

--- js/app.jsx
+++ js/app.jsx
@@ -1,28 +1,59 @@
-function Hero(props) {
-    return (
-        <div>
-        <img src={props.imageURL} />
-        <h1>{props.title}</h1>
-        <p>{props.subtitle}</p>
+// function Hero(props) {
+//     let count = 0
+//     function handleClick() {
+//         count =+ 1;
+//     }
+//     return (
+//         <div className="cointainer">
+//             <div className="count">{count}</div>
+//             <img src={props.imageURL} onClick={handleClick} />
+//             <h1>{props.title}</h1>
+//             <p>{props.subtitle}</p>
 
-    </div>
-    );
-}
+//     </div>
+//     );
+// }
 
+const Hero = React.createClass({
+    getInitialState: function(){
+        return {
+            count:0
+        };
+    },
+    handleClick: function() {
+        this.setState({
+            count: this.state.count + 1
+        });
+    },
+    render: function(){
+        return (
+            <div className="cointainer">
+                <div className="count">{this.state.count}</div>
+                <img src={this.props.imageURL} onClick={this.handleClick} />
+                <h1>{this.props.title}</h1>
+                <p>{this.props.subtitle}</p>
 
-// const jsx = 
-// <div>
-//     <img src="http://sourcingelectricals.net/wp-content/uploads/2017/12/Facebook-Logo-PNG-Clipart-copy-700x421.png" />
-//     <h1>Hello</h1>
-//     <p>React</p>
-
-// </div>;
-ReactDOM.render(<Hero 
+        </div> 
+        );
+    }
+});
+ReactDOM.render(
+    <div>
+            <Hero 
                 title = "React"
                 imageURL = "Facebook-Logo.png"
                 subtitle = "Библиотека"
                 
-                />, 
-    
+                
+                />
+                
+            <Hero 
+                title = "Angular"
+                imageURL = "Angular_full_color_logo.svg.png"
+                subtitle = "Библиотека для всего"
+                
+                
+            />    
+    </div>,
     
     document.getElementById('root'));
